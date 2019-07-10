108156ac05ef2334fe16c32f7716cf3ea47639e7
harshverma <harshv@cs.washington.edu>
2019-07-10 13:41:42 -0700

    Add appropriate app bar.

--- lib/map.dart
+++ lib/map.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
-
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 
 class MapScreen extends StatefulWidget {
   MapScreenState createState() {
@@ -11,98 +12,123 @@ class MapScreen extends StatefulWidget {
 }
 
 class MapScreenState extends State<MapScreen> {
+  static final CameraPosition myHome = CameraPosition(
+    target: LatLng(47.663645, -122.286446),
+    zoom: 14.4746,
+  );
 
-    static final CameraPosition myHome = CameraPosition(
-      target: LatLng(47.663645, -122.286446),
-      zoom: 14.4746,
+  GoogleMapController mapController;
+  Firestore firestore = Firestore.instance;
+  Geoflutterfire geo = Geoflutterfire();
+  Stream<List<DocumentSnapshot>> stream;
+  List<GeoPoint> markerList = new List<GeoPoint>();
+
+  void initState() {
+    GeoFirePoint center =
+        geo.point(latitude: 47.663645, longitude: -122.286446);
+    var collectionRef = firestore.collection('events');
+    double radius = 50;
+    String field = 'position';
+    stream = geo
+        .collection(collectionRef: collectionRef)
+        .within(center: center, radius: radius, field: field);
+    stream.listen(_updateMarkers);
+  }
+
+  Widget build(BuildContext context) {
+    final tabBar = new TabBar(
+      tabs: <Tab>[
+        new Tab(icon: new Icon(Icons.map)),
+        new Tab(icon: new Icon(Icons.chat)),
+        new Tab(icon: new Icon(Icons.person_pin_circle)),
+      ],
     );
 
-    GoogleMapController mapController;
-    Firestore firestore = Firestore.instance;
-    Geoflutterfire geo = Geoflutterfire();
-    Stream<List<DocumentSnapshot>> stream;
-    List<GeoPoint> markerList = new List<GeoPoint>();
-
-
-    void initState() {
-      GeoFirePoint center = geo.point(latitude: 47.663645, longitude: -122.286446);
-      var collectionRef = firestore.collection('events');
-      double radius = 50;
-      String field = 'position';
-      stream = geo.collection(collectionRef: collectionRef).within(
-          center: center, radius: radius, field: field);
-      stream.listen(_updateMarkers);
-    }
-
-    Widget build(BuildContext context) {
-      return Stack(
-        children: <Widget>[
-          GoogleMap(
-            initialCameraPosition: myHome,
-            onMapCreated: _onMapCreated,
-            markers: markerList.map((GeoPoint p) {
-              print('hey');
-              print(p.latitude);
-              print(markerList.length);
-              return new Marker(
-                  markerId: MarkerId(p.latitude.toString() + "," + p.longitude.toString()),
-                  position: LatLng(p.latitude, p.longitude),
-                  icon: BitmapDescriptor.defaultMarker);
-              }).toSet(),
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar:
+          AppBar(
+            bottom: tabBar,
+            title: Text('Cluster'),
           ),
-          Positioned(
-            bottom: 50,
-            right: 10,
-            child: FlatButton(
-                onPressed: _goToAddEventPage(context),
-                child: Icon(Icons.add)
+          body: TabBarView(physics: NeverScrollableScrollPhysics(), children: [
+            Stack(
+              children: <Widget>[
+                GoogleMap(
+                    initialCameraPosition: myHome,
+                    onMapCreated: _onMapCreated,
+                    markers: markerList.map((GeoPoint p) {
+                      print('hey');
+                      print(p.latitude);
+                      print(markerList.length);
+                      return new Marker(
+                        markerId: MarkerId(p.latitude.toString() +
+                            "," +
+                            p.longitude.toString()),
+                        position: LatLng(p.latitude, p.longitude),
+                        icon: BitmapDescriptor.defaultMarkerWithHue(20.0),
+                        flat: true,
+                      );
+                    }).toSet(),
+                    gestureRecognizers: Set()
+                      ..add(Factory<PanGestureRecognizer>(
+                          () => PanGestureRecognizer()))),
+                Positioned(
+                  bottom: 50,
+                  right: 10,
+                  child: FlatButton(
+                      onPressed: _goToAddEventPage(context),
+                      child: Icon(Icons.add)),
+                )
+              ],
             ),
-          )
-        ],
-      );
-    }
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ]),
+        ));
+  }
 
-    void _onMapCreated(GoogleMapController controller) {
-      setState(() {
-        mapController = controller;
-      });
-    }
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
 
-    Function _goToAddEventPage(BuildContext context) {
-      return () {
-        Navigator.pushNamed(context, '/addEvent');
-      };
-    }
+  Function _goToAddEventPage(BuildContext context) {
+    return () {
+      Navigator.pushNamed(context, '/addEvent');
+    };
+  }
 
-    void _updateMarkers(List<DocumentSnapshot> documentList) {
-      print(documentList);
-      bool needToRebuild = false;
-      if (documentList.length != markerList.length) {
-        print('yo');
-        needToRebuild = true;
-      }
-      if (!needToRebuild) {
-        print('la');
-        for (int i = 0; i < documentList.length; i++) {
-          GeoPoint pos1 = documentList[i].data['position']['geopoint'];
-          GeoPoint pos2 = markerList[i];
-          if ((pos1.latitude != pos2.latitude)
-              || (pos1.longitude != pos2.longitude)) {
-            needToRebuild = true;
-            break;
-          }
+  void _updateMarkers(List<DocumentSnapshot> documentList) {
+    print(documentList);
+    bool needToRebuild = false;
+    if (documentList.length != markerList.length) {
+      print('yo');
+      needToRebuild = true;
+    }
+    if (!needToRebuild) {
+      print('la');
+      for (int i = 0; i < documentList.length; i++) {
+        GeoPoint pos1 = documentList[i].data['position']['geopoint'];
+        GeoPoint pos2 = markerList[i];
+        if ((pos1.latitude != pos2.latitude) ||
+            (pos1.longitude != pos2.longitude)) {
+          needToRebuild = true;
+          break;
         }
       }
-      if (needToRebuild) {
-        setState(() {
-          markerList.clear();
-          documentList.forEach((doc) {
-            double latitude = doc.data['position']['geopoint'].latitude;
-            double longitude = doc.data['position']['geopoint'].longitude;
-            markerList.add(GeoPoint(latitude, longitude));
-          });
-        });
-      }
     }
-
+    if (needToRebuild) {
+      setState(() {
+        markerList.clear();
+        documentList.forEach((doc) {
+          double latitude = doc.data['position']['geopoint'].latitude;
+          double longitude = doc.data['position']['geopoint'].longitude;
+          markerList.add(GeoPoint(latitude, longitude));
+        });
+      });
+    }
+  }
 }
