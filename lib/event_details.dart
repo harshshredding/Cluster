ac375ec4dec4da4a23fe702ca2f2997a9b1ab4fb
Harsh Verma <harshshredding@gmail.com>
2019-07-19 15:28:19 -0700

    Add basic chat window. Add tab for chat

--- lib/event_details.dart
+++ lib/event_details.dart
@@ -6,43 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:http/http.dart' as http;
-
-class InfoView extends StatelessWidget {
-  final String id;
-
-  const InfoView(this.id);
-
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Text("hello"),
-              Spacer(),
-              Text("hello"),
-            ],
-          ),
-          SizedBox(height: 8),
-          Text("he"),
-          SizedBox(height: 8),
-          Text("he"),
-          SizedBox(height: 24),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Save'),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 class DetailsScreen extends StatefulWidget {
   final String id;
@@ -55,13 +18,50 @@ class DetailsScreen extends StatefulWidget {
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    DocumentReference document =
+        widget.firestore.collection('events').document(widget.id);
+
+    final tabBar = new TabBar(
+      tabs: <Tab>[
+        new Tab(icon: new Icon(Icons.description)),
+        new Tab(icon: new Icon(Icons.chat)),
+      ],
+    );
+
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar:  AppBar(
+          bottom: tabBar,
+        ),
+        body:
+        TabBarView(physics: NeverScrollableScrollPhysics(), children: [
+          DetailsInformationScreen(document),
+          Center(
+            child: Text("haha"),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+
+class DetailsInformationScreen extends StatelessWidget {
+
+  DocumentReference document;
+
+  DetailsInformationScreen(this.document);
 
   Widget getEventImage(snapshot) {
     if (snapshot.data['download_url'] == null) {
       return Container(
         child: ClipRRect(
           borderRadius: new BorderRadius.circular(10.0),
-          child: Image.asset("images/meWithLongHair.jpeg", fit: BoxFit.cover),
+          child: Image.asset("images/default_event.jpg", fit: BoxFit.cover),
         ),
         padding: EdgeInsets.all(8),
         height: 400,
@@ -82,28 +82,23 @@ class _DetailsScreenState extends State<DetailsScreen> {
     }
   }
 
-  @override
+  Widget createIfFieldExists(snapshot, String field, factory) {
+    if (snapshot.data[field] != null) {
+      print(snapshot.data[field]);
+      return factory(snapshot);
+    } else {
+      return Container(width: 0, height: 0);
+    }
+  }
+
   Widget build(BuildContext context) {
-    DocumentReference document =
-        widget.firestore.collection('events').document(widget.id);
-    return Scaffold(
-        body: FutureBuilder(
+    return FutureBuilder(
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return Container(
             padding: EdgeInsets.all(10),
             child: ListView(
               children: <Widget>[
-                Align(
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                    ),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                  alignment: Alignment.centerLeft,
-                ),
                 getEventImage(snapshot),
                 Card(
                   child: Column(
@@ -112,7 +107,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         alignment: Alignment.centerLeft,
                         margin: EdgeInsets.all(10.0),
                         child:
-                            createIfFieldExists(snapshot, 'title', (snapshot) {
+                        createIfFieldExists(snapshot, 'title', (snapshot) {
                           return Text(
                             snapshot.data['title'],
                             style: TextStyle(
@@ -125,18 +120,18 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         alignment: Alignment.centerLeft,
                         margin: EdgeInsets.all(20.0),
                         child: createIfFieldExists(snapshot, 'summary',
-                            (snapshot) {
-                          return Text(snapshot.data['summary'],
-                              style: TextStyle(
-                                  fontSize: 15.0, fontFamily: 'Heebo-Black'));
-                        }),
+                                (snapshot) {
+                              return Text(snapshot.data['summary'],
+                                  style: TextStyle(
+                                      fontSize: 15.0, fontFamily: 'Heebo-Black'));
+                            }),
                         padding: EdgeInsets.all(4),
                       ),
                       Container(
                         alignment: Alignment.centerLeft,
                         margin: EdgeInsets.all(20.0),
                         child:
-                            createIfFieldExists(snapshot, 'date', (snapshot) {
+                        createIfFieldExists(snapshot, 'date', (snapshot) {
                           return Text(snapshot.data['date'],
                               style: TextStyle(
                                   fontSize: 15.0, fontFamily: 'Heebo-Black'));
@@ -147,7 +142,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         alignment: Alignment.centerLeft,
                         margin: EdgeInsets.all(20.0),
                         child:
-                            createIfFieldExists(snapshot, 'time', (snapshot) {
+                        createIfFieldExists(snapshot, 'time', (snapshot) {
                           return Text(snapshot.data['time'],
                               style: TextStyle(
                                   fontSize: 15.0, fontFamily: 'Heebo-Black'));
@@ -158,11 +153,11 @@ class _DetailsScreenState extends State<DetailsScreen> {
                         alignment: Alignment.centerLeft,
                         margin: EdgeInsets.all(20.0),
                         child: createIfFieldExists(snapshot, 'address',
-                            (snapshot) {
-                          return Text(snapshot.data['address'],
-                              style: TextStyle(
-                                  fontSize: 15.0, fontFamily: 'Heebo-Black'));
-                        }),
+                                (snapshot) {
+                              return Text(snapshot.data['address'],
+                                  style: TextStyle(
+                                      fontSize: 15.0, fontFamily: 'Heebo-Black'));
+                            }),
                         padding: EdgeInsets.all(4),
                       )
                     ],
@@ -178,15 +173,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
         }
       },
       future: document.get(),
-    ));
-  }
-
-  Widget createIfFieldExists(snapshot, String field, factory) {
-    if (snapshot.data[field] != null) {
-      print(snapshot.data[field]);
-      return factory(snapshot);
-    } else {
-      return Container(width: 0, height: 0);
-    }
+    );
   }
 }
+
